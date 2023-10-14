bb1023dd262cd61678fd9c0964c648ab56399743
Wallace S. Wilson <wsw954@gmail.com>
2023-10-14 15:53:39 -0400

    Commit 1: Initial Set up of basic folders

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import NavBar from "/components/global/NavBar";
+import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <NavBar />
+      </div>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Bizzle</h1>
+        <p className={styles.description}>
+          CSA-1F Index
+        </p>
+        <p className={styles.description}></p>
+        <div></div>
+      </main>
+    </div>
+  );
+}
